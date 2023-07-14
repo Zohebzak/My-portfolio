f9f81b5b4db10ab7cac0d48a462e6b78678ce98d
Zoheb khan <Zohabkhan8856@gmail.com>
2023-07-14 15:22:54 +0530

    ..

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../components/pdf/Zoheb resume.pdf";
+import pdf from "../../components/pdf/Zoheb_resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Zoheb_resume.pdf";
+  "https://raw.githubusercontent.com/zohebzak/portfolio/master/src/Assets/Zoheb_resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
